f1e7b7376e65e069cd55b7585c5ed6d446c7bdc7
Marcin Ciesiński <casin1903@gmail.com>
2019-04-05 14:43:44 +0200

    add models to app

--- mysite/crud/models.py
+++ mysite/crud/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Dzial(models.Model):
+  name = models.CharField(max_length=25)
+  department_code = models.CharField(max_length=5)
+
+class Pracownik(models.Model):
+  dzial = models.ForeignKey(Dzial, on_delete=models.CASCADE)
+  first_name = models.CharField(max_length=15)
+  last_name = models.CharField(max_length=25)
+  phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+  email = models.EmailField()
+  position = models.CharField(max_lenght=50)
