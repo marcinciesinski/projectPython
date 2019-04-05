bd6824afa3b825159459589112734668ff170e77
Marcin Ciesiński <casin1903@gmail.com>
2019-04-05 14:28:46 +0200

    add phone number field to model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'phonenumber_field'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
