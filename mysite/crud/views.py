301635b452a9aa5a5e8a665531015ad8e0d49d9b
Marcin Ciesiński <casin1903@gmail.com>
2019-04-08 15:02:10 +0200

    dodanie metody dzial_details pokazująca liste pracownikow

--- mysite/crud/views.py
+++ mysite/crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Dzial, Pracownik
 
@@ -9,3 +9,8 @@ def dzialy(request):
   dzial_list = Dzial.objects.all().order_by('pk')
   context = {'dzial_list': dzial_list}
   return render(request, 'crud/dzialy.html', context)
+
+def dzial_details(request, dzial_id):
+  dzial = get_object_or_404(Dzial, pk = dzial_id)
+  context = {'dzial': dzial}
+  return render(request, 'crud/dzialDetails.html', context)
