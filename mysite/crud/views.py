f3f34aa5995448f5107f9c4d593dd4a13a2636de
Marcin Ciesiński <casin1903@gmail.com>
2019-04-08 14:18:07 +0200

    zmiana HttpResponse na render i dodanie nowego widoku dzialy

--- mysite/crud/views.py
+++ mysite/crud/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Dzial, Pracownik
 
 def index(request):
-  return HttpResponse("Pierwsza apka Django!")
+  return render(request, 'crud/index.html')
+
+def dzialy(request):
+  dzial_list = Dzial.objects.all().order_by('pk')
+  context = {'dzial_list': dzial_list}
+  return render(request, 'crud/dzialy.html', context)
