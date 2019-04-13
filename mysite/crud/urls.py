3c8c34ab514ae6a3e412ace464936ef43b086552
Marcin Ciesiński <casin1903@gmail.com>
2019-04-13 20:21:45 +0200

    dodanie sciezek do widokow

--- mysite/crud/urls.py
+++ mysite/crud/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name = 'index'),
-  path('dzialy/', views.dzialy, name = 'dzialy')
+  path('dzialy/', views.dzialy, name = 'dzialy'),
+  path('dzialy/<int:dzial_id>/', views.dzial_details, name = 'dzial_details'),
+  path('dzialy/<int:dzial_id>/delete', views.dzial_delete, name = 'dzial_delete'),
 ]
