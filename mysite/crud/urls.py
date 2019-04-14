5d6b0bc6fc8d805267f3caa81e54896a5d8ffbd6
Marcin Ciesiński <casin1903@gmail.com>
2019-04-14 21:44:22 +0200

    dodanie metod i url dla pracownika

--- mysite/crud/urls.py
+++ mysite/crud/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
   path('dzialy/add', views.dzial_add, name = 'dzial_add'),
   path('dzialy/add/new', views.dzial_add_new, name = 'dzial_add_new'),
   path('dzialy/<int:dzial_id>/edit', views.dzial_edit, name = 'dzial_edit'),
-  path('dzialy/dzial_edit/<int:dzial_id>', views.dzial_to_edit, name = 'dzial_to_edit')
+  path('dzialy/dzial_edit/<int:dzial_id>', views.dzial_to_edit, name = 'dzial_to_edit'),
+  path('dzialy/add/<int:dzial_id>/prac', views.prac_add, name = 'prac_add'),
+  path('dzialy/add/new/prac/<int:dzial_id>', views.dzial_add_new_prac, name = 'dzial_add_new_prac'),
+
+  path('dzialy/<int:dzial_id>/edit/prac/<int:prac_id>', views.prac_edit, name = 'prac_edit'),
+  path('dzialy/<int:dzial_id>/prac_edit/prac/<int:prac_id>', views.prac_to_edit, name = 'prac_to_edit'),
+  path('dzialy/<int:dzial_id>/delete/prac/<int:prac_id>', views.prac_delete, name = 'prac_delete'),
 ]
--- mysite/crud/views.py
+++ mysite/crud/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from .models import Dzial, Pracownik
 
+  # Metody wyświetlające zawartosc
+
 def index(request):
   return render(request, 'crud/index.html')
 
@@ -16,6 +18,8 @@ def dzial_details(request, dzial_id):
   dzial = get_object_or_404(Dzial, pk = dzial_id)
   context = {'dzial': dzial}
   return render(request, 'crud/dzialDetails.html', context)
+
+  # Metody CRUD do modelu Dzial
 
 def dzial_delete(request, dzial_id):
   dzial = get_object_or_404(Dzial, pk = dzial_id)
@@ -54,4 +58,52 @@ def dzial_to_edit(request, dzial_id):
       return render(request, 'crud/dzialy.html',{'error_message_edit': "Coś poszło nie tak... spróbuj ponownie."})
   else:
     return render(request, 'crud/dzialy.html',{'error_message_edit': "Coś poszło nie tak... spróbuj ponownie."})
+
+  # METODY CRUD do modelu Pracownik
+
+def prac_delete(request, prac_id, dzial_id):
+  prac = get_object_or_404(Pracownik, pk = prac_id)
+  dzial = get_object_or_404(Dzial, pk = dzial_id)
+  context = {'prac': prac, 'dzial': dzial}
+  prac.delete()
+  return render(request, 'crud/pracDelete.html', context)
+
+def prac_add(request, dzial_id):
+  dzial = get_object_or_404(Dzial, pk = dzial_id)
+  return render (request, 'crud/pracAdd.html',{'dzial': dzial})
+
+def dzial_add_new_prac(request, dzial_id):
+  if request.method == 'POST':
+    dzial = get_object_or_404(Dzial, pk = dzial_id)
+    prac_to_add = dzial.pracownik_set.create(
+                              first_name = request.POST['pracownikName'],
+                              last_name = request.POST['pracownikLastName'],
+                              phone_number = request.POST['pracownikPhoneNum'],
+                              email = request.POST['pracownikEmail'],
+                              position = request.POST['pracownikPosition'])
+    
+    prac_to_add.save()
+    return HttpResponseRedirect(reverse('crud:dzial_details', args=[dzial_id]))
+  else:
+    return render(request, 'crud/pracAdd.html',{'error_message_add': "Coś poszło nie tak... spróbuj ponownie."})
+
+def prac_edit(request, dzial_id, prac_id):
+  dzial = get_object_or_404(Dzial, pk = dzial_id)
+  prac = dzial.pracownik_set.get(pk = prac_id)
+  context = {'dzial': dzial, 'prac': prac}
+  return render(request, 'crud/pracEdit.html', context)
+
+def prac_to_edit(request, dzial_id, prac_id):
+  if request.method == 'POST':
+    prac_to_edit = get_object_or_404(Pracownik, pk = prac_id)
+    prac_to_edit.dzial = request.POST['dzialId']
+    prac_to_edit.first_name = request.POST['pracownikName']
+    prac_to_edit.last_name = request.POST['pracownikLastName']
+    prac_to_edit.phone_number = request.POST['pracownikPhoneNum']
+    prac_to_edit.email = request.POST['pracownikEmail']
+    prac_to_edit.position = request.POST['pracownikPosition']
+    prac_to_edit.save()
+    return HttpResponseRedirect(reverse('crud:dzial_details', args=[dzial_id]))
+
+
 
